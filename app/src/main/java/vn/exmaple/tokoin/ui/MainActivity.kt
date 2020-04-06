c96b921fc24b68124f15750de52a693d6e63319d
Hoang Duy, Pham <devduyph@gmail.com>
2020-04-06 17:40:37 +0700

    update new ui

--- app/src/main/java/vn/exmaple/tokoin/ui/MainActivity.kt
+++ app/src/main/java/vn/exmaple/tokoin/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package vn.exmaple.tokoin.ui
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,16 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home,
-            R.id.navigation_dashboard,
-            R.id.navigation_notifications
-        ))
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
